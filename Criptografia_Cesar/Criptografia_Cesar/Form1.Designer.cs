c0c3a1c57e8de1d6c9dd9d741a819a312f7973fe
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2020-04-14 15:56:30 -0300

    criptografia Cesar

---
+++ Criptografia_Cesar/Criptografia_Cesar/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Criptografia_Cesar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.encriptar = new System.Windows.Forms.Button();
+            this.Limpar = new System.Windows.Forms.Button();
+            this.decriptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(430, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(430, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // encriptar
+            // 
+            this.encriptar.Location = new System.Drawing.Point(139, 234);
+            this.encriptar.Name = "encriptar";
+            this.encriptar.Size = new System.Drawing.Size(75, 23);
+            this.encriptar.TabIndex = 3;
+            this.encriptar.Text = "encriptar";
+            this.encriptar.UseVisualStyleBackColor = true;
+            this.encriptar.Click += new System.EventHandler(this.encriptar_Click);
+            // 
+            // Limpar
+            // 
+            this.Limpar.Location = new System.Drawing.Point(139, 205);
+            this.Limpar.Name = "Limpar";
+            this.Limpar.Size = new System.Drawing.Size(156, 23);
+            this.Limpar.TabIndex = 4;
+            this.Limpar.Text = "Limpar";
+            this.Limpar.UseVisualStyleBackColor = true;
+            this.Limpar.Click += new System.EventHandler(this.Limpar_Click);
+            // 
+            // decriptar
+            // 
+            this.decriptar.Location = new System.Drawing.Point(220, 234);
+            this.decriptar.Name = "decriptar";
+            this.decriptar.Size = new System.Drawing.Size(75, 23);
+            this.decriptar.TabIndex = 5;
+            this.decriptar.Text = "decriptar";
+            this.decriptar.UseVisualStyleBackColor = true;
+            this.decriptar.Click += new System.EventHandler(this.decriptar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 293);
+            this.Controls.Add(this.decriptar);
+            this.Controls.Add(this.Limpar);
+            this.Controls.Add(this.encriptar);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button encriptar;
+        private System.Windows.Forms.Button Limpar;
+        private System.Windows.Forms.Button decriptar;
+    }
+}
+
---
+++ Criptografia_Cesar/Criptografia_Cesar/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Criptografia_Cesar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Limpar_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+        }
+
+        private void encriptar_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = string.Empty;
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                int txtUsuario = (int)textBox1.Text[i];
+                int txtCifrado = txtUsuario + 10;
+                textBox3.Text += char.ConvertFromUtf32(txtCifrado);
+            }
+        }
+
+        private void decriptar_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = string.Empty;
+            for (int i = 0; i < textBox3.Text.Length; i++)
+            {
+                int txtCifrado = (int)textBox3.Text[i];
+                int txtOriginal = txtCifrado - 10;
+                textBox2.Text += char.ConvertFromUtf32(txtOriginal);
+            }
+        }
+    }
+}
